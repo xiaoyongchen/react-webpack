69b23dff98caadb0f6708cc30a29ef7c1556c7c2
陈小勇 <xiaoyong.chen@zhangmen.com>
2020-04-16 16:47:37 +0800

    htmlWebpack使用

--- node.md
+++ node.md
@@ -13,3 +13,12 @@ npm install webpack webpack-cli -D
 # 打包
 
 npx webpack
+
+# scripts 脚本配置之后 -- 前面不需要要写这个
+
+npm run build -- --config webpack.config.js
+
+# Html-webpack 使用使用本地模版 html 生成在 dist 下的 html 文件, dev-server 使用 express,在内存中打包，解析到内存中
+
+npm install webpack-dev-server --save-dev
+npm install --save-dev html-webpack-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: 'development',
+    mode: 'production', // production || development
     entry: './src/index.js', // 入口，相对路径
     output: {
-        filename: 'bundle.js', // 打包后文件名，默认main.js
+        filename: 'bundle.[hash:8].js', // 打包后文件名，默认main.js,hash每次生成的build不一样。
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    devServer: {
+        open: true,
+        port: 3000,
+        progress: true, // 进度条
+        contentBase: './dist', // 不使用默认内存中的，制定文件夹
+        compress: true, // gzip压缩
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 需要使用的模版
+            filename: 'index.html',
+            // 在生产上生成dist/index.html一些压缩
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+            },
+            hash: true, // <script 后面build.js?后面的哈希值
+        }),
+    ]
+
 }
